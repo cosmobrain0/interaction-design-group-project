729419ea3379cf755f6a387d515be4af9e2bc78c
adam-whittome <aw2095@cam.ac.uk>
2025-05-15 15:43:35 +0100

    Created tabs for home, settings and calendar pages.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabsLayout() {
+  return <Stack>
+    <Stack.Screen
+      name="(tabs)"
+      options={{
+        headerShown: false
+      }}
+    />
+  </Stack>;
 }
