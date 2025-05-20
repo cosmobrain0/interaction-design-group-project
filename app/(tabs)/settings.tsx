6c3bbfd8fc4c84a61c39ec100455cf54f7c82514
cosmobrain0 <cosmobrain@outlook.com>
2025-05-20 15:02:46 +0100

    all of those switches now work

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -7,14 +7,41 @@ import { Text, View, Image, StyleSheet, Switch } from "react-native"
 export default function Settings() {
   const [settingsLoading, setSettingsLoading] = useState(false);
   const [notificationsEnabled, setNotificationsEnabled] = useState(false); // TODO: read from file or something
-  const toggleNotifications = () => setNotificationsEnabled(!notificationsEnabled);
+  const [newsAlertsEnabled, setNewsAlertsEnabled] = useState(false);
+  const [cloudCoverAlertsEnabled, setCloudCoverAlertsEnabled] = useState(false);
+
+  const toggleNotifications = () => {
+    setNotificationsEnabled(!notificationsEnabled);
+    setNewsAlertsEnabled(!notificationsEnabled);
+    setCloudCoverAlertsEnabled(!notificationsEnabled);
+  }
+
+  const toggleNewsAlerts = () => {
+    setNewsAlertsEnabled(!newsAlertsEnabled);
+    const newNewsAlertsEnabled = !newsAlertsEnabled;
+    if (newNewsAlertsEnabled && cloudCoverAlertsEnabled && !notificationsEnabled) {
+      setNotificationsEnabled(true);
+    } else if ((!newNewsAlertsEnabled || !cloudCoverAlertsEnabled) && notificationsEnabled) {
+      setNotificationsEnabled(false);
+    }
+  }
+
+  const toggleCloudCoverageAlerts = () => {
+    setCloudCoverAlertsEnabled(!cloudCoverAlertsEnabled);
+    const newCloudCoverAlertsEnabled = !cloudCoverAlertsEnabled;
+    if (newsAlertsEnabled && newCloudCoverAlertsEnabled && !notificationsEnabled) {
+      setNotificationsEnabled(true);
+    } else if ((!newsAlertsEnabled || !newCloudCoverAlertsEnabled) && notificationsEnabled) {
+      setNotificationsEnabled(false);
+    }
+  }
 
   return <View style={[Styles.container, Styles.background]}>
     <View style={Styles.informationColumn}>
       <View style={[Styles.container]}>
         <ToggleSetting name="Notifications" onValueChange={toggleNotifications} value={notificationsEnabled} />
-        <ToggleSetting name="News Alerts" onValueChange={toggleNotifications} value={notificationsEnabled} />
-        <ToggleSetting name="Cloud Cover Alerts" onValueChange={toggleNotifications} value={notificationsEnabled} />
+        <ToggleSetting name="News Alerts" onValueChange={toggleNewsAlerts} value={newsAlertsEnabled} />
+        <ToggleSetting name="Cloud Cover Alerts" onValueChange={toggleCloudCoverageAlerts} value={cloudCoverAlertsEnabled} />
       </View>
     </View>
   </View>
