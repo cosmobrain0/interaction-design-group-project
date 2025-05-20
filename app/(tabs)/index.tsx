fcc0ea9cc868eb7e9f3abe88e3a21f83f168187e
Magikarp4000 <1fishyblub1@gmail.com>
2025-05-20 13:58:47 +0100

    something

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export default function Home() {
   return <View style={styles.container}>
