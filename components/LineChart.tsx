34b2ce9dc01a1c357a3ceceb14395afde4cb1d2e
Xiawuuu <qs238@cam.ac.uk>
2025-05-22 15:53:41 +0100

    fetchapi data based on the location selected by the user(with bugged linechart)

--- components/LineChart.tsx
+++ components/LineChart.tsx
@@ -1,7 +1,7 @@
 import { Colors } from "@/constants/Colors";
 import { Styles } from "@/constants/Styles";
 import { curveMonotoneX } from "d3-shape";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { LineChart as Chart, Grid, XAxis, YAxis } from "react-native-svg-charts";
 
 export function LineChart({ chartData, chartLabels }: any) {
@@ -27,12 +27,17 @@ export function LineChart({ chartData, chartLabels }: any) {
         />
       </Chart>
     </View>
-    <XAxis
+    {/*<XAxis
       style={{ width:'100%', marginBottom: 5 }}
       data={chartData}
-      formatLabel={(_: any, index: number) => index % 4 === 0 ? chartLabels[index].slice(0, 2) : ''}
+      formatLabel={(value, index) => {
+        const label = chartLabels?.[index];
+        const result = index % 4 === 0 && typeof label === 'string' ? label.slice(0, 2) : '';
+        console.log(`Label[${index}]:`, result);
+        return result;
+      }}
       contentInset={{ left: 18, right: 0 }}
       svg={{ fontSize: 10, fill: Colors.foregroundSecondary }}
-    />
+    />*/}
   </View>
 }
